e0eed9fa86c80ef3340a61a10f0d25bd710e8085
Adam Hatherly <adam.hatherly@hscic.gov.uk>
2016-09-23 16:54:28 +0100

    Initial commit

---
+++ apis.php
@@ -0,0 +1,66 @@
+ <?php
+/*
+Template Name: API Hub
+*/
+?>
+
+<?php get_header(); ?>
+		
+
+<div role="main" class="main">
+	<div class="page_title">
+		<div class="wrapper">
+			<hgroup>
+				<h1>API Hub</h1>
+				<h2>Explore and Make use of Nationally Defined Messaging APIs</h2>
+			</hgroup>
+		</div>
+	</div><!--end page title-->
+	
+	<div class="main_content cf">
+		<?php include_once "breadcrumbs.php"; ?>
+
+		<div id="api_list">
+		
+			<div class="wrapper cf container">
+				
+				<h2 class="api_type_titles">FHIR APIs</h2>
+				<?php if( get_field('fhir_apis') ): ?>
+				<?php while( has_sub_field('fhir_apis') ): ?>
+				<div class="one_third tree_wrap">
+					<div class="box">
+						<div class="line aquaBlue_line"></div>
+						<section class="nav_box aquablue">
+							<h3><a href="<?php the_sub_field('url'); ?>"><?php the_sub_field('capability_pack'); ?></a></h3>
+							<p><?php the_sub_field('description'); ?></p>
+						</section>
+					</div>
+				</div>
+				<?php endwhile; ?>
+				<?php endif; ?>
+			
+			</div>
+			<div class="wrapper cf container">
+				
+				<h2 class="api_type_titles">SMSP APIs</h2>
+				<?php if( get_field('smsp_apis') ): ?>
+				<?php while( has_sub_field('smsp_apis') ): ?>
+				<div class="one_third tree_wrap">
+					<div class="box">
+						<div class="line aquaBlue_line"></div>
+						<section class="nav_box aquablue">
+							<h3><a href="<?php the_sub_field('url'); ?>"><?php the_sub_field('capability_pack'); ?></a></h3>
+							<p><?php the_sub_field('description'); ?></p>
+						</section>
+					</div>
+				</div>
+				<?php endwhile; ?>
+				<?php endif; ?>
+					
+				</div><!--end content wrap-->
+			</div><!--end wrapper-->
+		</div><!-- end api list -->
+	</div><!--end main_content-->
+</div><!-- end main -->
+		
+<?php get_footer(); ?>
---
+++ footer.php
@@ -0,0 +1,26 @@
+		<footer>
+			<div class="wrapper cf">
+				<nav>
+					<ul class="cf">
+						<li><a href="/accessibility">Accessibility</a></li>
+						<li><a href="/privacy">Privacy</a></li>
+						<li><a href="/cookie-policy">Cookie Policy</a></li>
+					</ul>
+				</nav>
+				
+				<p><a target="_blank" href="http://www.nationalarchives.gov.uk/information-management/our-services/crown-copyright.htm" class="copyright">&copy; Crown Copyright <?php echo date("Y") ?></a></p>
+			</div><!--end wrapper-->
+		</footer>
+
+		<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+		<script type="text/javascript">stLight.options({publisher: "983c2eed-07b4-4136-abb6-dae9ce5acde4", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+		
+		<script src="<?php bloginfo('template_directory'); ?>/js/plugins-ck.js"></script>
+		<script src="<?php bloginfo('template_directory'); ?>/js/script-ck.js"></script>
+		<?php wp_footer(); ?> 
+		
+	</body>
+</html>
