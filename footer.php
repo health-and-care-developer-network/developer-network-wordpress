cf238c1333fccac99da624fb6d78776790b4aad9
Darren <darren@develer.co.uk>
2017-03-29 14:17:14 +0100

    removal of strange script

--- footer.php
+++ footer.php
@@ -17,7 +17,6 @@
 		<script type="text/javascript">stLight.options({publisher: "983c2eed-07b4-4136-abb6-dae9ce5acde4", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-		<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 
         <script src="<?php bloginfo('template_directory'); ?>/js/landing/promise.min.js"></script>
         <script src="<?php bloginfo('template_directory'); ?>/js/landing/fetch.min.js"></script>
