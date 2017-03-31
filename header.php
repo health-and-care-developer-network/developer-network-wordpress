0760ff62496b7f06ec87c8f972e8cc4f725ae732
Darryl Waterhouse <pinpointzero@gmail.com>
2017-03-31 09:56:30 +0100

    Added API Hub.

--- header.php
+++ header.php
@@ -11,7 +11,13 @@
             </div>
         </div>
         <ul>
-            <li class="header__menu-item"><a href="/library/">Library</a></li><li class="header__menu-item"><a href="/downloads-data">Download &amp; Data</a></li><li class="header__menu-item"><a href="/testcentre">Test centre</a></li><li class="header__menu-item"><a href="/learn">Learn</a></li><li class="header__menu-item"><a href="/community">Community</a></li><li class="header__menu-item"><a href="/health-apps">Health Apps</a></li>
+            <li class="header__menu-item"><a href="/library/">Library</a></li>
+            <li class="header__menu-item"><a href="/downloads-data">Download &amp; Data</a></li>
+            <li class="header__menu-item"><a href="/apis">API Hub</a></li>
+            <li class="header__menu-item"><a href="/testcentre">Test centre</a></li>
+            <li class="header__menu-item"><a href="/learn">Learn</a></li>
+            <li class="header__menu-item"><a href="/community">Community</a></li>
+            <li class="header__menu-item"><a href="/health-apps">Health Apps</a></li>
         </ul>
     </nav>
     <div class="header__search-area">
