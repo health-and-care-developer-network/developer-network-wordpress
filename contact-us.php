df8e75e84c57c078e7031636c6fa2b39dd2f426d
Darryl Waterhouse <pinpointzero@gmail.com>
2017-07-31 12:29:26 +0100

    Initial new contact-us page.

--- contact-us.php
+++ contact-us.php
@@ -5,7 +5,7 @@
 ?>
 <?php get_header(); ?>
 
-<div role="main" class="main digital-tools-style">
+<div role="main" class="main contact-us-style">
     <div class="page_title">
         <div class="wrapper">
             <hgroup>
@@ -38,46 +38,16 @@
                   <h2 class="large_space">Frequently Asked Questions</h2>
 
                     <div class="faq_content">
-                        <?php hdnPosts::the_faqs('landing'); ?>
+                        <?php hdnPosts::the_faqs('contact-us'); ?>
                     </div>
 
-                    <h1>The Digital Tools Library Assessment Path</h1>
+                </div>
 
-                    <p>The current NHS Digital Tools Library (<a href="https://apps.beta.nhs.uk">apps.beta.nhs.uk</a>) is a beta site, which means that it is in development.</p>
-                    <p>We have developed a collection of standards and guidance information to help you develop digital tools which meet the principles of quality applied to all digital products. This means your digital tool will meet what we consider to be good quality digital standards for safety, experience and effectiveness. </p>
-                    <p>All digital tools applying to go on the NHS Digital Tools Library must be assessed equally and thoroughly through the current process framework. </p>
+                <div class="content_wrap">
+                  <h2 class="large_space">Contact Us Form </h2>
 
-                    <h4>Assessment Process and Questions to Produce Trusted Content</h4>
-
-                    <p>To get an idea of the standards, rules and regulations against which digital health tools are assessed you can view the <a href="/digital-tools/daq">Digital Assessment Questionnaire – Beta</a>. For now, this is only a guide to the assessment question criteria we are using. If you feel that you would be able to comply with the assessment, or would be near to complying, please register your interest as a developer. Once the next steps have been defined we will contact you with further information.</p>
-
-                    <div class="assessment_path">
-                        <div class="link_section">
-                            <a href="#">
-                                <div class="link_container">
-                                    <div class="review_icon"></div>
-                                    <span>Review Assessment Questions</span>
-                                </div>
-                            </a>
-                            <a href="#">Review Assessment Questions &gt;</a>
-                        </div>
-                        <div class="arrow_section">
-                        </div>
-                        <div class="link_section">
-                            <a href="#">
-                                <div class="link_container">
-                                    <div class="register_icon"></div>
-                                    <span>Register Your Interest</span>
-                                </div>
-                            </a>
-                            <a href="#">Register Your Interest &gt;</a>
-                        </div>
-                    </div>
-
-                    <h2 class="large_space">Frequently Asked Questions</h2>
-
-                    <div class="faq_content">
-                        <?php hdnPosts::the_faqs('landing'); ?>
+                    <div class="large_space">
+                      <?php echo FrmFormsController::show_form(51, $key = '', $title=false, $description=false); ?>
                     </div>
 
                 </div>
